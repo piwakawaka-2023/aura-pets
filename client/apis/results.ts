e2d4aac9a4c73775dd04843b360b1f34834a6a7a
Jackson Simpson <Jackson7890air@gmail.com>
2023-06-29 15:34:48 +1200

    Add router-setup

---
+++ client/apis/results.ts
@@ -0,0 +1,5 @@
+import request from 'superagent'
+
+const resultUrl = '/api/v1/result'
+
+export async function fetchResult()
---
+++ client/router.tsx
@@ -0,0 +1,20 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from 'react-router-dom'
+
+// Component Imports
+
+import App from './components/App'
+import Home from './components/Home'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<Home />} />
+    </Route>
+  )
+)
+
+export default router
