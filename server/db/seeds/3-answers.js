7e039b6fdd1873580bc588d24a112ddc03460656
Kayne Paranihi <14kayne12@gmail.com>
2023-06-29 16:30:49 +1200

    start answers seeds

--- server/db/seeds/3-answers.js
+++ server/db/seeds/3-answers.js
@@ -4,10 +4,8 @@
  */
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+  await knex('answers').del()
+  await knex('answers').insert([
+    {a_id: 1, answer: 'rowValue1', q_id: 1},
   ]);
 };
