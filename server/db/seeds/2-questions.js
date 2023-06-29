72a9f079336e4a08fa5077abed084b079c844fdf
Kayne Paranihi <14kayne12@gmail.com>
2023-06-29 16:28:48 +1200

    update question  id for futureproofing

--- server/db/seeds/2-questions.js
+++ server/db/seeds/2-questions.js
@@ -4,10 +4,17 @@
  */
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+  await knex('questions').del()
+  await knex('questions').insert([
+    {q_id: 8001, question: `What is your Preferred way to spend the weekend?`},
+    {q_id: 8002, question: `If you could have any 1 superpower or ability, what would it be?` },
+    {q_id: 8003, question: `What's your ideal holiday destination?` },
+    {q_id: 8004, question: `What is your favorite genre of fictional storytelling?` },
+    {q_id: 8005, question: `If you were a celebrity, what would be known for?` },
+    {q_id: 8006, question: `If you could talk to any historical figure, who would it be?` },
+    {q_id: 8007, question: `If you were stranded on a desert island, which item would you bring with you?` },
+    {q_id: 8008, question: `What item from your wardrobe do you like the most?` },
+    {q_id: 8009, question: `What type of game show would you be a contestant in?` },
+    {q_id: 8010, question: `If you could make a secret base / hideout anywhere, where would it be?` }
   ]);
 };
