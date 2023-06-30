9531cb9b829335f8619b5a8bca3ef36d5b93da99
Johnathan-Spicer <johnathanspicer335@gmail.com>
2023-06-30 14:49:02 +1200

    hello

--- models/types.ts
+++ models/types.ts
@@ -1,5 +1,5 @@
 export interface Question {
-  id: number
+  qId: number
   question: string
 }
 
@@ -11,12 +11,7 @@ export interface Answer {
   petName: string
 }
 
-<<<<<<< HEAD
 export interface ResultData {
-=======
-export interface Result {
-  id: number
->>>>>>> 5a3426f613091c4664bb0dd1e82c94a87aaf7bc7
   name: string
   sprite: string
   bio: string
