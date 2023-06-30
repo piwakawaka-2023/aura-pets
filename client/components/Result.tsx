2ee2224f3fdda361722ef144c0fa060dda24f542
Jackson Simpson <Jackson7890air@gmail.com>
2023-06-30 17:30:08 +1200

    Fixed Results page - pet route in back-end

--- client/components/Result.tsx
+++ client/components/Result.tsx
@@ -1,20 +1,28 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { fetchResult } from '../apis/results'
-import { Result } from '../../models/types'
+import * as type from '../../models/types'
 
-function Results() {
-  const { result } = useParams()
-  const [animals, setAnimals] = useState({} as Result)
+function Result() {
+  const { id } = useParams()
+  const [pet, setPet] = useState({} as type.Result)
+
   useEffect(() => {
     async function resultPromsie() {
-      const apiRes = await fetchResult(Number(result))
-      setAnimals(apiRes)
+      const res = await fetchResult(Number(id))
+      setPet(res)
     }
     resultPromsie()
-  }, [result])
+  }, [id])
 
-  return <></>
+  return (
+    <>
+      <h1>YOU GOT:</h1>
+      <h2>{pet.name}</h2>
+      <img src={`/imgs/${pet.sprite}`} alt={pet.name}></img>
+      <p>{pet.bio}</p>
+    </>
+  )
 }
 
-export default Results
+export default Result
