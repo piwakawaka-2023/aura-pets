3c9c406a07acd70b57ca1debf1955e5f7b111dcf
Kayne Paranihi <14kayne12@gmail.com>
2023-07-03 16:36:37 +1200

    add temporary no-auth workaround

--- client/index.tsx
+++ client/index.tsx
@@ -1,3 +1,30 @@
+// //import { Provider } from 'react-redux'
+// import { createRoot } from 'react-dom/client'
+// import { RouterProvider } from 'react-router-dom'
+// import { router } from './router'
+// import store from './store'
+// import { Auth0Provider } from '@auth0/auth0-react' // added
+// import App from './components/App'
+// import { Provider } from 'react-redux'
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   createRoot(document.getElementById('app') as HTMLElement).render(
+//     <Auth0Provider
+//       domain="piwakwaka-2023-james.au.auth0.com"
+//       clientId="UeEZBYf1faTRYtFSwqEjjAvIK3zTgt6A"
+//       redirectUri={window.location.origin}
+//       audience="https://pets/api"
+      
+//     >
+//       <Provider store={store}>
+
+//       <RouterProvider router={router} />
+//       <App />
+//       </Provider>
+//     </Auth0Provider>
+//   )
+// })
+
 //import { Provider } from 'react-redux'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
@@ -9,18 +36,10 @@ import { Provider } from 'react-redux'
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
-    <Auth0Provider
-      domain="piwakwaka-2023-james.au.auth0.com"
-      clientId="UeEZBYf1faTRYtFSwqEjjAvIK3zTgt6A"
-      redirectUri={window.location.origin}
-      audience="https://pets/api"
-      
-    >
       <Provider store={store}>
 
       <RouterProvider router={router} />
       <App />
       </Provider>
-    </Auth0Provider>
   )
 })
