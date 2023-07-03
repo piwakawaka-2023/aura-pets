5767250edd8f178a99761893352bdbe6389109b1
Jackson Simpson <Jackson7890air@gmail.com>
2023-07-04 10:05:46 +1200

    Commented out Auth0 on homepage - sent to Fac's for review

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -2,28 +2,29 @@ import { Link } from 'react-router-dom'
 import { User, useAuth0 } from '@auth0/auth0-react'
 
 function Home() {
+  // const { user, logout, loginWithRedirect } = useAuth0()
 
-  const { user, logout, loginWithRedirect } = useAuth0()
+  // const handleSignIn = () => {
+  //   return loginWithRedirect()
+  // }
 
-  const handleSignIn = () => {
-    return loginWithRedirect()
-  }
-
-    return (
-      <>
-     <div className="centered-container">
-      <div className="home-div">
-       <h1 className="welcome-message">Welcome to Aura Pets</h1>
-       <h2 className="home-message">Click the button below to find out your Aura Pet</h2>
-       <button className="quiz-btn-centered">
-         <Link to="/Quiz">
-           <button onClick={handleSignIn}>Sign In</button>
-         </Link>
-       </button>
+  return (
+    <>
+      <div className="centered-container">
+        <div className="home-div">
+          <h1 className="welcome-message">Welcome to Aura Pets</h1>
+          <h2 className="home-message">
+            Click the button below to find out your Aura Pet
+          </h2>
+          <button className="quiz-btn-centered">
+            <Link to="/Quiz">
+              <button onClick={handleSignIn}>Sign In</button>
+            </Link>
+          </button>
+        </div>
       </div>
-     </div>
-      </>
-    )
-  }
-  
-  export default Home
+    </>
+  )
+}
+
+export default Home
