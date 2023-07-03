96b5747a9ed1618dd47d0a3ac892f5e28781bbb0
Kayne Paranihi <14kayne12@gmail.com>
2023-07-03 16:35:40 +1200

    add user seed to be cleaned

--- server/db/seeds/0-clean.js
+++ server/db/seeds/0-clean.js
@@ -1,6 +1,7 @@
 exports.seed = (knex) => {
   const empty = (table) => knex(table).delete()
   return empty('answers')
+  .then(() => empty('chat'))
   .then(() => empty('users'))
   .then(() => empty('pets'))
   .then(() => empty('questions'))
