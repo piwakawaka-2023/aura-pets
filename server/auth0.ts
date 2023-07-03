2216cca61b984fb6af60cc0f486d4339116d57b1
Jackson Simpson <Jackson7890air@gmail.com>
2023-07-03 15:07:06 +1200

    Add back in route

---
+++ server/auth0.ts
@@ -0,0 +1,27 @@
+import { expressjwt as jwt, GetVerificationKey } from 'express-jwt'
+import type { Request } from 'express'
+import type { ParamsDictionary } from 'express-serve-static-core'
+import type { JwtPayload } from 'jsonwebtoken'
+import jwks from 'jwks-rsa'
+
+const domain = 'piwakwaka-2023-james.au.auth0.com'
+const audience = 'https://pets/api'
+
+const checkJwt = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `${domain}/.well-known/jwks.json`,
+  }) as GetVerificationKey,
+  audience: audience,
+  issuer: `${domain}/`,
+  algorithms: ['RS256'],
+})
+
+export default checkJwt
+
+export interface JwtRequest<TReq = any, TRes = any>
+  extends Request<ParamsDictionary, TRes, TReq> {
+  auth?: JwtPayload
+}
--- server/db/result.ts
+++ server/db/result.ts
@@ -3,6 +3,6 @@ import { Result } from '../../models/types'
 
 const db = connection
 
-export function getPet(id: number): Promise<Result[]> {
+export function getResult(id: number): Promise<Result[]> {
   return db('pets').select().where({ id })
 }
