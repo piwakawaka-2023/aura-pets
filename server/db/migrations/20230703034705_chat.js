2728aca623a0c97043106fbc0d2673beddc79c9e
Kayne Paranihi <14kayne12@gmail.com>
2023-07-03 16:08:23 +1200

    add chat table migration

---
+++ server/db/migrations/20230703034705_chat.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('chat', (table) => {
+    table.increments('id').primary()
+    table.string('user_id').references('users.id')
+    table.string('message')
+    table.timestamp('created_at').defaultTo(Date.now())
+
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('chat')
+};
