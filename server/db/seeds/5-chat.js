2f18c3172b677bf992932db70397589d3b30da52
Kayne Paranihi <14kayne12@gmail.com>
2023-07-03 16:23:51 +1200

    rename table name in seed to chats

---
+++ server/db/seeds/5-chat.js
@@ -0,0 +1,10 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> } 
+ */
+exports.seed = async function(knex) {
+  await knex('chat').insert([
+    {id: 1, user_id: '1', message:'helloooo'},
+    {id: 2, user_id: '1', message:'Anybody wanna battle?'},
+  ]);
+};
