ccb9fa0217a92f06fbcd2ca6cedbead92a01c138
james-bellion <belliondjames@hotmail.co.uk>
2023-07-03 12:16:24 +1200

    set up auth on website made changes to client/index routes

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,6 +1,14 @@
 import { Link } from 'react-router-dom'
+import { User, useAuth0 } from '@auth0/auth0-react'
 
 function Home() {
+
+  const { user, logout, loginWithRedirect } = useAuth0()
+
+  const handleSignIn = () => {
+    return loginWithRedirect()
+  }
+
     return (
       <>
      <div className="centered-container">
@@ -9,7 +17,7 @@ function Home() {
        <h2 className="home-message">Click the button below to find out your Aura Pet</h2>
        <button className="quiz-btn-centered">
          <Link to="/Quiz">
-           <li>Quiz</li>
+           <button onClick={handleSignIn}>Sign In</button>
          </Link>
        </button>
       </div>
