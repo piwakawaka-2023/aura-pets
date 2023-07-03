6b97e4a13e70667ea994f43655eb3a3192f4279a
Kayne Paranihi <14kayne12@gmail.com>
2023-07-03 16:37:02 +1200

    start db func for chat

---
+++ server/db/chat.ts
@@ -0,0 +1,9 @@
+import connection from './connection'
+import {Answer}  from '../../models/types'
+const db = connection 
+
+export function getAllMessages() {
+  return db('chats')
+  .join('users', 'chats.user_id', 'users.id')
+  .select('chats.id','user_id as userId', 'message', 'created_at', 'users.pet_nickname as petNickName', )
+}
