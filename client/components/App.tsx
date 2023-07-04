19757f7e5caed5e38e2812aa085ff5454bf63351
Jackson Simpson <Jackson7890air@gmail.com>
2023-07-04 15:35:41 +1200

    Playing around changes

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,9 +1,19 @@
-import { Outlet } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+
+import Home from './Home'
+import Quiz from './Quiz'
+import Result from './Result'
+import Profile from './Profile'
 
 function App() {
   return (
     <>
-      <Outlet />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/result/:id" element={<Result />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </>
   )
 }
--- client/index.tsx
+++ client/index.tsx
@@ -1,20 +1,22 @@
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './router'
-import store from './store'
 import { Auth0Provider } from '@auth0/auth0-react'
 import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
+import store from './store'
+import App from './components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
     <Auth0Provider
       domain="piwakawaka-2023-jackson.au.auth0.com"
       clientId="wE1L4rhAEQ4FcxQ25Be0erjEcvdOKLcr"
-      authorizationParams={window.location.origin}
+      redirectUri={window.location.origin}
       audience="https://aura-pets/api"
     >
       <Provider store={store}>
-        <RouterProvider router={router} />
+        <Router>
+          <App />
+        </Router>
       </Provider>
     </Auth0Provider>
   )
