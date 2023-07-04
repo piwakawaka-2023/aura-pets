37bc6cc14bdf080629767c7bc5b42f8e771dff65
Jackson Simpson <Jackson7890air@gmail.com>
2023-07-05 10:40:04 +1200

    Removed unrequired redux files

--- models/actions.ts
+++ models/actions.ts
@@ -1,8 +1,5 @@
-import { Question, Answer, UpdateUserInfo } from './types'
+import { Question, Answer } from './types'
 
 export type QuestionActions = { type: 'SET_QUESTIONS'; payload: Question[] }
 export type AnswerActions = { type: 'SET_ANSWERS'; payload: Answer[] }
 export type TallyAction = { type: 'INCREMENT'; payload: string }
-export type UserActions =
-  | { type: 'ERROR'; payload: string }
-  | { type: 'UPDATE_PROFILE'; payload: UpdateUserInfo }
